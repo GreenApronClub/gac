7fb86a81b1487c679f4ea2626e7ef1fca9f6f3c1
JingaFoo <calvinmwariama@yahoo.com>
2018-11-21 22:05:43 -0500

    refactor actionLogin

--- client/src/actions/login/actionLogin.js
+++ client/src/actions/login/actionLogin.js
@@ -1,22 +1,20 @@
 import axios from 'axios';
 import * as types from '../actionTypes';
 
-const ROOT_URL = "http://localhost:8080";
-export var IsLoggedIn = false;
+export const IsLoggedIn = false;
 
-export function requestLogin(values, props) {
-  const request = axios.post(`${ROOT_URL}/login`, values)
-
+export default function requestLogin(values, props) {
+  const ROOT_URL = 'http://localhost:8080';
+  const request = axios.post(`${ROOT_URL}/login`, values);
   return (dispatch) => {
-    request.then(response => {
+    request.then((response) => {
       sessionStorage.setItem('jwt', response.data.token);
-      if(response.data.isLoggedIn === true && sessionStorage.getItem('jwt')) {
+      if (response.data.isLoggedIn === true && sessionStorage.getItem('jwt')) {
         dispatch({ type: types.IS_LOGGED_IN, payload: response.data.isLoggedIn });
         props.history.push('/active-orders');
       }
-    })
-    .catch(error => {
+    }).catch((error) => {
       dispatch({ type: types.LOGIN_ERROR, payload: error.response.data });
-    })
-  }
+    });
+  };
 }
